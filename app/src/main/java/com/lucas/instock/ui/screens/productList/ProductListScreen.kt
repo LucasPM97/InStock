53f79e36b4651324835eac73c6c57bc5a3cc6353
Lucas Perera Montanari <42821366+LucasPM97@users.noreply.github.com>
2022-09-19 16:04:50 -0300

    Hilt first implementation on UI (working for now)

---
+++ app/src/main/java/com/lucas/instock/ui/screens/productList/ProductListScreen.kt
@@ -0,0 +1,10 @@
+package com.lucas.instock.ui.screens.productList
+
+import androidx.compose.runtime.Composable
+import androidx.lifecycle.viewmodel.compose.viewModel
+
+@Composable
+fun ProductListScreen(
+    viewModel: ProductListViewModel = viewModel()
+) {
+}
